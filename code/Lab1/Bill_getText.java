2f1be6fcb61f0b67d17b4fd246995cd2be9b8784
Tazsjah B. <tblanto1@my.westga.edu>
2025-08-21 12:54:50 -0400

    Updated Class & Functions
    1) Created a new class for the Bill:getText() method
    2) Removed the getText() method from the Bill object class

---
+++ code/Lab1/Bill_getText.java
@@ -0,0 +1,34 @@
+package edu.westga.cs1302.lab1.view;
+
+import edu.westga.cs1302.lab1.model.Bill;
+import edu.westga.cs1302.lab1.model.BillItem;
+
+public class Bill_getText {
+	
+	/** Return a String containing the list of bill items and total for the bill.
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return a String containing the list of bill items and total for the bill
+	 */
+	public String getText(Bill bill) {
+		String text = "ITEMS" + System.lineSeparator();
+		double subTotal = 0.0;
+		for (BillItem item : bill.getItems()) {
+			text += item.getName() + " - " + item.getAmount() + System.lineSeparator();
+			subTotal += item.getAmount();
+		}
+		
+		text += System.lineSeparator();
+		text += "SUBTOTAL - $" + subTotal + System.lineSeparator();
+		double tax = subTotal * 0.1;
+		double tip = subTotal * 0.2;
+		text += "TAX - $" + tax + System.lineSeparator();
+		text += "TIP - $" + tip + System.lineSeparator();
+		text += "TOTAL - $" + (subTotal + tip + tax);
+		
+		return text;
+	}
+
+}
