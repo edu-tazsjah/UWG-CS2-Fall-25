ae90f71e16c18c3c67b5a207f5a00f86b1f4b262
Tazsjah <34932124+Tazsjah@users.noreply.github.com>
2025-09-24 01:01:50 -0400

    Added UI for Adding Tasks
    1) Added labels & associated controls for the UI
    2) Setup the MainWindow class for the UI

--- code/Project1/src/main/java/edu/westga/cs1302/project1/views/MainWindow.java
+++ code/Project1/src/main/java/edu/westga/cs1302/project1/views/MainWindow.java
@@ -1,5 +1,9 @@
 package edu.westga.cs1302.project1.views;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -8,6 +12,11 @@ package edu.westga.cs1302.project1.views;
  * @version Fall 2025
  */
 public class MainWindow {
+	
+	private TextField newTaskName;
+	private TextArea newTaskDescription;
+	private ComboBox<String> newTaskPriority;
+	private Button newTaskAddButton;
     
     /**
      * Perform any needed initialization of UI components and underlying objects.
@@ -15,4 +24,14 @@ public class MainWindow {
     public void initialize() {
     	
     }
+    
+    /**
+     * This adds a new task to the task list
+     */
+    public void addNewTask() {
+    	
+    	/* This is the setup for the UI */
+    	
+    }
+    
 }
