282c5ba4dc429d61289412029bd6a791dae752f9
Tazsjah <34932124+Tazsjah@users.noreply.github.com>
2025-09-25 17:21:51 -0400

    Adding in Task Model
    1) Adding the Task model, but is unfinished.

--- code/Project1/src/main/java/edu/westga/cs1302/project1/model/Task.java
+++ code/Project1/src/main/java/edu/westga/cs1302/project1/model/Task.java
@@ -1,5 +1,58 @@
 package edu.westga.cs1302.project1.model;
 
 public class Task {
+	
+	private String name;
+	private String description;
+	private String priority;
+	
+	/** Creates a new Task model
+	 * 
+	 * @precondition name != null
+	 * 				!name.isEmpty
+	 * 				priority != null
+	 * 				!priority.isEmpty
+	 * @postcondition none
+	 * 
+	 * @param name of the Task model
+	 * @param description of the Task model
+	 * @param priority of the Task model
+	 * 
+	 * @throws IllegalArgumentException
+	 */
+	public Task(String name, String description, String priority) {
+		if (name == null || name.isEmpty()) {
+			throw new IllegalArgumentException("Task name cannot be null!");
+		}
+		
+		if (priority == null || priority.isEmpty()) {
+			throw new IllegalArgumentException("Task priority cannot be null!");
+		}
+		
+		this.name = name;
+		this.description = description;
+		this.priority = priority;
+	}
+	
+	/** Returns the name of the Task model
+	 * 
+	 * @return string name of the Task
+	 * 
+	 */
+	public String toString() {
+		return this.name;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public String getPriority() {
+		return this.priority;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
